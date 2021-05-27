63e43b4a38c3dea7b385c0b30546c4f49ab26aad
Max Daunarovich <max.daunarovich@gmail.com>
2021-05-27 03:39:33 +0300

    Add header. Refactor constants to the top

--- src/parser.js
+++ src/parser.js
@@ -1,4 +1,31 @@
+/*
+ * Flow JS Testing
+ *
+ * Copyright 2021 Dapper Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { collapseSpaces } from "./strings";
+
+export const CONTRACT = "contract";
+export const TRANSACTION = "transaction";
+export const SCRIPT = "script";
+export const UNKNOWN = "unknown";
+
+const contractMatcher = /\w+\s+contract\s+(\w*\s*)\w*/g;
+const transactionMatcher = /transaction(\(\s*\))*\s*/g;
+const scriptMatcher = /pub\s*fun\s*main\s*/g;
 
 export const extract = (code, keyWord) => {
   const target = code
@@ -27,20 +54,13 @@ export const extractTransactionArguments = (code) => {
   return extract(code, "transaction");
 };
 
-const contractMatcher = /\w+\s+contract\s+(\w*\s*)\w*/g;
-const transactionMatcher = /transaction(\(\s*\))*\s*/g;
-const scriptMatcher = /pub\s*fun\s*main\s*/g;
-
-export const CONTRACT = "contract";
-export const TRANSACTION = "transaction";
-export const SCRIPT = "script";
-export const UNKNOWN = "unknown";
-
 export const getTemplateInfo = (code) => {
   if (code.match(contractMatcher)) {
     return {
       type: CONTRACT,
       signers: 1,
+      // TODO: implement extraction from `init` method
+      args: []
     };
   }
 
