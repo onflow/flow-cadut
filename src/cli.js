474d51cc6e97ff22faa95cc6edf300b72b6675e0
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-17 14:22:52 +0300

    Refactor CLI to use yargs

--- src/cli.js
+++ src/cli.js
@@ -16,11 +16,63 @@
  * limitations under the License.
  */
 
-import { processFolder } from "./processor";
+import fs from "fs";
+import simpleGit from "simple-git";
+import yargs from "yargs";
+
+import { processFolder, processGitRepo } from "./processor";
 import "./templates";
+
+// Initially we will support only GitHub repos
+// TODO: support other urls. List can be found here:
+// https://stackoverflow.com/questions/2514859/regular-expression-for-git-repository
+const isGitUrl = (input) => /https:\/\/github.com/.test(input);
+
+export const parseArgs = (argv) => {
+  let input, output;
+  let branch = argv.branch;
+
+  switch (argv._.length) {
+    case 2: {
+      input = argv._[0];
+      output = argv._[1];
+      break;
+    }
+    case 1: {
+      input = argv._[0];
+      output = argv.output;
+      break;
+    }
+    default: {
+      input = argv.input;
+      output = argv.output;
+    }
+  }
+
+  if (argv._.length === 2) {
+    input = argv._[0];
+    output = argv._[1];
+  }
+
+  return { input, output, branch };
+};
 
 export async function run(args) {
   const hideBin = args.slice(2);
-  const [input, output, ixDependency] = hideBin;
-  await processFolder(input, output, { ixDependency });
+
+  const argv = require("yargs/yargs")(hideBin)
+    .alias("i", "input")
+    .alias("o", "output")
+    .alias("b", "branch")
+    .default({ i: "./cadence", o: "./src/generated", dependency: "flow-js-testing" }).argv;
+  // console.log(argv)
+  const { input, output, branch } = parseArgs(argv);
+  console.log({ input, output, branch });
+
+  if (isGitUrl(input)) {
+    await processGitRepo(input, output, branch, { dependency });
+  } else {
+    fs.rmdirSync(output, { recursive: true });
+    await processFolder(input, output, { dependency });
+  }
 }
