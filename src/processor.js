57e814071cda74a6510f482bb660b547a0db58f1
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-17 14:22:17 +0300

    Remove backticks from templates. Move refactorParams method to processor file. create processGitRepo method.

--- src/processor.js
+++ src/processor.js
@@ -16,11 +16,58 @@
  * limitations under the License.
  */
 
+import fs from "fs";
 import { resolve } from "path";
 import Handlebars from "handlebars";
+import simpleGit from "simple-git";
+
 import { getSplitCharacter, trimAndSplit, underscoreToCamelCase } from "./strings";
 import { generateExports, getFilesList, readFile, writeFile } from "./file";
 import { getTemplateInfo, CONTRACT, SCRIPT, TRANSACTION, extractSigners } from "./parser";
+
+const extractParams = (input, branch) => {
+  // eslint-disable-next-line no-useless-escape
+  const groups = /(\w+:\/\/)(.+@)*([\w\d\.]+)(:[\d]+)?\/*(.*)/g.exec(input);
+
+  // TODO: use branch argument to get actual folder value
+
+  const inputPath = groups[5];
+  const inputBits = inputPath.split("/");
+  const pathBits = inputBits.slice(4);
+  const folderPath = pathBits.join("/");
+
+  const [owner, repo] = inputBits;
+  const branchValue = inputBits[3];
+  const fetchUrl = `https://github.com/${owner}/${repo}`;
+  return { folderPath, fetchUrl, branchValue };
+};
+
+const TEMP_REPO_FOLDER = "./temp-generator-repo";
+const clean = () => {
+  fs.rmdirSync(TEMP_REPO_FOLDER, { recursive: true });
+};
+
+export const processGitRepo = async (input, output, branch, cliOptions = {}) => {
+  const git = simpleGit({
+    baseDir: process.cwd(),
+    binary: "git",
+  });
+
+  const { fetchUrl, folderPath } = extractParams(input, branch);
+
+  clean();
+
+  const options = ["--single-branch", "--depth", "1"];
+  if (branch) {
+    options.concat(["--branch", branch]);
+  }
+
+  await git.clone(fetchUrl, TEMP_REPO_FOLDER, options);
+  await processFolder(`${TEMP_REPO_FOLDER}/${folderPath}`, output, cliOptions );
+
+  // Teardown
+  clean();
+};
 
 export const processFolder = async (input, output, options = {}) => {
   const splitCharacter = getSplitCharacter(input);
@@ -29,13 +76,19 @@ export const processFolder = async (input, output, options = {}) => {
 
   for (let i = 0; i < fileList.length; i++) {
     const path = fileList[i];
+
+    // Skip all but Cadence template files
+    if (!path.endsWith(".cdc")) {
+      continue;
+    }
+
     const packages = trimAndSplit(path, fullBasePath);
     const pathPackages = packages.slice(0, -1);
     const file = packages.slice(-1)[0];
 
-    const ixDependency = options.ixDependency || "flow-js-testing";
+    const ixDependency = options.dependency || "flow-js-testing";
 
-    const code = readFile(path);
+    const code = readFile(path).replace(/`/g, "'");
     const name = underscoreToCamelCase(file.replace(".cdc", ""));
 
     const templateInfo = getTemplateInfo(code);
