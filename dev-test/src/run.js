8631f8d6af1dbed4faa0fcd0d26445f32a69a4af
Max Daunarovich <max.daunarovich@gmail.com>
2021-06-27 01:35:34 +0300

    Fix run example

--- dev-test/src/run.js
+++ dev-test/src/run.js
@@ -6,29 +6,18 @@ import { init } from "./utils";
 (async () => {
   init();
   const payer = authorization();
-  /*
-  const [result, err] = await flow.transactions.log({ signers: [payer], payer, proposer: payer });
-  console.log({ result, err });
-   */
 
-  /*
-  // Deploy Basic contract
-  const code = await flow.contracts.BasicTemplate()
-  console.log({code})
+  const [txResult, txErr] = await flow.transactions.log({ signers: [payer], payer, proposer: payer });
+  console.log({ txResult, txErr });
 
-  const [result, err] = await deployContract({
-    name: "Basic",
-    to: payer,
-    update: false,
-    code,
-  });
+
+  const [result, err] = await flow.contracts.deployBasic({to: payer, update: true})
   console.log({ result, err });
-  */
 
   // Query Basic
   const addressMap = {
     Basic: "0xf8d6e0586b0a20c7",
   };
-  const [result, err] = await flow.scripts.basic({ addressMap });
-  console.log({ result, err });
+  const [message] = await flow.scripts.basic({ addressMap });
+  console.log({ message });
 })();
