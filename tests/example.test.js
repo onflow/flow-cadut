40d84e6b302821c74caaa5af346896aab8fed37c
Max Daunarovich <max.daunarovich@gmail.com>
2021-09-18 01:43:16 +0300

    fix test name

--- tests/example.test.js
+++ tests/example.test.js
@@ -119,7 +119,7 @@ describe("documentation examples", function () {
     }
   });
 
-  it("should convert values to sdk arguments", function () {
+  it("should convert values to sdk arguments - code", function () {
     const code = `
       pub fun main(metadata: {String:String}, key: String):String {
         return metadata[key]!
