56a99b27f34bc43d1d0f2abfb8271ae2cf17efb7
Max Daunarovich <max.daunarovich@gmail.com>
2021-11-17 01:54:55 +0300

    Add utility methods

--- src/plugins.js
+++ src/plugins.js
@@ -15,3 +15,21 @@ export const registerPlugin = ({ type, id, callback }) => {
   });
   console.log({ plugins });
 };
+
+export const getPlugins = (type) => {
+  const plugins = config.get("ix.plugins") || {};
+  return plugins[type] && plugins[type].length > 0;
+};
+
+export const applyPlugins = async (props, plugins) => {
+  let type = props.type;
+  let value = props.value;
+  for (let i = 0; i < plugins.length; i++) {
+    const { callback } = plugins[i];
+    const result = await callback(type, value);
+    type = result.type;
+    value = result.value;
+  }
+
+  return { type, value };
+};
