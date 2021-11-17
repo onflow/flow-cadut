a22dd7e02c6d3a4aaefb4cd04fab0d783908642c
Max Daunarovich <max.daunarovich@gmail.com>
2021-11-17 03:46:10 +0300

    Refactor plugin registration

--- src/plugins.js
+++ src/plugins.js
@@ -1,32 +1,37 @@
 import { config } from "@onflow/fcl";
 
-export const registerPlugin = ({ type, id, callback }) => {
-  const plugins = config.get("ix.plugins") || {};
-  const typedPlugins = plugins[type] || [];
-  config.set("ix.plugins", {
-    ...plugins,
-    [type]: [
-      ...typedPlugins,
-      {
-        id,
-        callback,
-      },
-    ],
-  });
-  console.log({ plugins });
+export const PLUGIN_TYPES = {
+  ARGUMENT: "argument",
 };
 
-export const getPlugins = (type) => {
-  const plugins = config.get("ix.plugins") || {};
-  return plugins[type] && plugins[type].length > 0;
+export const registerPlugin = async (plugin) => {
+  const { type, id } = plugin;
+  const plugins = (await config().get("ix.plugins")) || {};
+  const typedPlugins = plugins[type] || [];
+
+  await config().put("ix.plugins", {
+    ...plugins,
+    [type]: [...typedPlugins, plugin],
+  });
+};
+
+export const getPlugins = async (type) => {
+  const plugins = (await config().get("ix.plugins")) || {};
+  const byType = plugins[type];
+
+  if (byType && byType.length > 0) {
+    return byType;
+  }
+  return false;
 };
 
 export const applyPlugins = async (props, plugins) => {
   let type = props.type;
   let value = props.value;
+
   for (let i = 0; i < plugins.length; i++) {
-    const { callback } = plugins[i];
-    const result = await callback(type, value);
+    const { resolver } = plugins[i];
+    const result = await resolver(type, value);
     type = result.type;
     value = result.value;
   }
