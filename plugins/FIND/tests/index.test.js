dc7ffeb92df4a9af081f64d7d973bd1c2cf1e36e
Max Daunarovich <max.daunarovich@gmail.com>
2021-11-17 03:48:44 +0300

    Add tess

---
+++ plugins/FIND/tests/index.test.js
@@ -0,0 +1,29 @@
+import { setEnvironment, registerPlugin, mapValuesToCode } from "../../../src";
+import { FIND } from "../src/index";
+
+describe("FIND plugin", () => {
+  beforeEach(async () => {
+    await setEnvironment("testnet");
+    await registerPlugin(FIND);
+  });
+
+  it("shall resolve find:name properly", async () => {
+    const code = `
+      pub fun main(address: Address): Address {
+        return address
+      }
+    `;
+    const args = ["find:bjarte"];
+    return mapValuesToCode(code, args);
+  });
+
+  it("shall resolve name.find properly", async () => {
+    const code = `
+      pub fun main(address: Address): Address {
+        return address
+      }
+    `;
+    const args = ["bjarte.find"];
+    return mapValuesToCode(code, args);
+  });
+});
