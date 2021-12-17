4c69e9ab48f843f0a3af29e5f5dd39e2626c24c8
Maksim Daunarovich <max.daunarovich@dapperlabs.com>
2021-12-17 14:30:38 +0300

    Added NonFungible token address (#46)

--- src/env.js
+++ src/env.js
@@ -26,6 +26,7 @@ export const DEPLOYED_CONTRACTS = {
   testnet: {
     FlowToken: "0x7e60df042a9c0868",
     FungibleToken: "0x9a0766d93b6608b7",
+    NonFungibleToken: "0x631e88ae7f1d7c20",
     LockedTokens: "0x95e019a17d0e23d7",
     StakingProxy: "0x7aad92e5a0715d21",
     FUSD: "0xe223d8a629e49c68",
@@ -33,6 +34,7 @@ export const DEPLOYED_CONTRACTS = {
   mainnet: {
     FlowToken: "0x1654653399040a61",
     FungibleToken: "0xf233dcee88fe0abe",
+    NonFungibleToken: "0x1d7e57aa55817448",
     LockedTokens: "0x8d0e87b65159ae63",
     StakingProxy: "0x62430cf28c26d095",
     FUSD: "0x3c5959b568896393",
