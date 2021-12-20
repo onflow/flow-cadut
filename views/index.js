52c5973fbd4312eb04f36dcfab75fcfad96c73ee
Max Daunarovich <max.daunarovich@gmail.com>
2021-12-20 03:38:34 +0300

    Export methods and strategies from index file

--- views/index.js
+++ views/index.js
@@ -1,18 +1,4 @@
-export const prepareView = async (strategy, address) => {
-  const { mapData, getData } = strategy(address);
+export { getView, prepareView } from "./manage";
 
-  return {
-    resolve: async () => {
-      const [data, err] = await getData();
-      if (err) {
-        return [null, err];
-      }
-      return [data.map(mapData), null];
-    },
-  };
-};
-
-export const getView = async (strategy, address) => {
-  const view = await prepareView(strategy, address);
-  return view.resolve();
-};
+export { default as VersusArt } from "./Versus";
+export { default as Flovatars } from "./Flovatars";
