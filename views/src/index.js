1ae114a1e18671d178dc48d8d92023cbb973f3dd
Max Daunarovich <max.daunarovich@gmail.com>
2021-12-27 05:15:26 +0300

    Implement Ballerz view

--- views/src/index.js
+++ views/src/index.js
@@ -4,3 +4,4 @@ export { makeViewHook } from "./hooks";
 export { default as VersusArt } from "./Versus";
 export { default as Flovatars } from "./Flovatars";
 export { default as MotoGP } from "./MotoGP";
+export { default as Ballerz } from "./Ballerz";
