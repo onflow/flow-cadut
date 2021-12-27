94bcef7ce421cd4b0c5bacafc40b08dfc710369b
Maksim Daunarovich <max.daunarovich@dapperlabs.com>
2021-12-27 05:16:43 +0300

    Implement Ballerz view (#49)
    * Add Ballerz test
    
    * Implement Ballerz view
    
    * Bump version

--- dev-test/test/try-views.test.js
+++ dev-test/test/try-views.test.js
@@ -3,12 +3,13 @@ import { getView } from "../../views/src";
 import Flovatars from "../../views/src/Flovatars";
 import VersusArt from "../../views/src/Versus";
 import MotoGP from "../../views/src/MotoGP";
+import Ballerz from "../../views/src/Ballerz";
+
 
 describe("views", () => {
   it("fetch flowvatars", async () => {
     await setEnvironment("mainnet");
     const [flowvatars, err] = await getView(Flovatars, "0x886f3aeaf848c535");
-    const [art] = await getView(VersusArt, "0x886f3aeaf848c535");
     console.log({ flowvatars });
     console.error(err);
   });
@@ -25,5 +26,12 @@ describe("views", () => {
     const [cards, err] = await getView(MotoGP, "0x53f389d96fb4ce5e");
     console.log({ cards });
     console.error(err);
+  });
+
+  it("fetch ballerz", async () => {
+    await setEnvironment("mainnet");
+    const [ballerz, err] = await getView(Ballerz, "0x4c342b6dafb5bcb1");
+    console.log({ ballerz });
+    err && console.error(err);
   });
 });
