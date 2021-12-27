d0007a1fcffa3980f2a445382f70d8d151687c38
Maksim Daunarovich <max.daunarovich@dapperlabs.com>
2021-12-27 17:25:19 +0300

    Add getDisplay method (#51)
    * Add getDisplay method
    
    * Refactor code and tests
    
    * Bump version

--- dev-test/test/try-views.test.js
+++ dev-test/test/try-views.test.js
@@ -1,10 +1,9 @@
 import { setEnvironment } from "../../src";
-import { getView } from "../../views/src";
+import { getView, getDisplay } from "../../views/src";
 import Flovatars from "../../views/src/Flovatars";
 import VersusArt from "../../views/src/Versus";
 import MotoGP from "../../views/src/MotoGP";
 import Ballerz from "../../views/src/Ballerz";
-
 
 describe("views", () => {
   it("fetch flowvatars", async () => {
@@ -33,5 +32,35 @@ describe("views", () => {
     const [ballerz, err] = await getView(Ballerz, "0x4c342b6dafb5bcb1");
     console.log({ ballerz });
     err && console.error(err);
+  });
+});
+
+describe("displays", () => {
+  it("shall fetch multiple panels at once", async () => {
+    await setEnvironment("mainnet");
+    const display = await getDisplay([Flovatars, VersusArt], "0x886f3aeaf848c535");
+    console.log(display);
+    expect(display.Flovatar).not.toBe(null);
+    expect(display.VersusArt).not.toBe(null);
+  });
+
+  it("shall fire changes asynchronously", async () => {
+    await setEnvironment("mainnet");
+
+    const resolved = {};
+
+    const views = [Flovatars, VersusArt, Ballerz];
+    const owner = "0x886f3aeaf848c535";
+    const onChange = (name) => {
+      resolved[name] = true;
+    };
+    const display = await getDisplay(views, owner, onChange);
+    expect(display.Flovatar).not.toBe(null);
+    expect(display.VersusArt).not.toBe(null);
+    expect(display.Ballerz).not.toBe(null);
+
+    expect(resolved.Flovatar).toBe(true);
+    expect(resolved.VersusArt).toBe(true);
+    expect(resolved.Ballerz).toBe(true);
   });
 });
--- views/src/manage.js
+++ views/src/manage.js
@@ -1,7 +1,8 @@
 export const prepareView = async (view, address) => {
-  const { mapData, getData } = view(address);
+  const { mapData, getData, name } = view(address);
 
   return {
+    name,
     blink: async () => {
       const [data, err] = await getData();
       if (err) {
@@ -15,4 +16,34 @@ export const prepareView = async (view, address) => {
 export const getView = async (view, address) => {
   const eyes = await prepareView(view, address);
   return eyes.blink();
+};
+
+export const resolveView = async (preparedView, onChange) => {
+  const { name, blink } = preparedView;
+  const [data] = await blink();
+  onChange(name, data);
+};
+
+export const getDisplay = async (views, address, onChange = null) => {
+  const panels = [];
+  for (let i = 0; i < views.length; i++) {
+    const view = views[i];
+    const panel = await prepareView(view, address);
+    panels.push(panel);
+  }
+
+  const promised = await Promise.all(
+    panels.map(async ({ name, blink }) => {
+      const [data] = await blink();
+      if (onChange) {
+        onChange(name, data);
+      }
+      return data;
+    })
+  );
+
+  return promised.reduce((acc, item, i) => {
+    acc[panels[i].name] = item;
+    return acc;
+  }, {});
 };
