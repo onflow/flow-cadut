8315ee156520bde2b46a78cd77bd5488106665cd
Jordan Ribbink <jribbink@users.noreply.github.com>
2022-08-05 13:45:55 -0700

    PKG -- [flow-cadut] Add support for optional complex types (#113)

--- packages/flow-cadut/src/args.js
+++ packages/flow-cadut/src/args.js
@@ -29,8 +29,10 @@ import {
   isArray,
   isDictionary,
   isComplexType,
-  wrongType,
+  isWrongType,
   isBasicNumType,
+  stripType,
+  isOptional,
 } from "./type-checker"
 
 import {removeSpaces} from "./strings"
@@ -99,15 +101,17 @@ export const reportMissing = (
 export const raw = type => type.slice(0, -1)
 
 export const resolveBasicType = type => {
-  if (wrongType(type)) return false
-
-  if (type.includes("?")) {
-    return t.Optional(t[raw(type)])
-  }
+  if (!t[type]) throwTypeError(`type ${type} is not supported`)
   return t[type]
 }
 
 export const resolveType = type => {
+  if (isWrongType(type)) throwTypeError("type is not a string")
+
+  if (isOptional(type)) {
+    return t.Optional(resolveType(stripType(type)))
+  }
+
   if (isComplexType(type)) {
     switch (true) {
       case isArray(type): {
--- packages/flow-cadut/src/type-checker.js
+++ packages/flow-cadut/src/type-checker.js
@@ -16,17 +16,20 @@
  * limitations under the License.
  */
 
-export const wrongType = type => !type || typeof type != "string"
+export const stripType = type => type.replace(/\s/g, "").replace(/\?$/, "")
+
+export const isWrongType = type => !type || typeof type !== "string"
+export const isOptional = type => /\?$/.test(type)
 
 export const isBasicNumType = type => {
-  if (wrongType(type)) return false
+  if (isWrongType(type)) return false
   return (
     type.startsWith("Int") || type.startsWith("UInt") || type.startsWith("Word")
   )
 }
 
 export const isFixedNumType = type => {
-  if (wrongType(type)) return false
+  if (isWrongType(type)) return false
   return type.startsWith("Fix64") || type.startsWith("UFix64")
 }
 
@@ -34,20 +37,19 @@ export const isString = type => type === "String"
 export const isCharacter = type => type === "Character"
 export const isBoolean = type => type === "Bool"
 export const isAddress = type => type === "Address" || type === "Address?"
-export const isPath = type =>
-  type === "Path" ||
-  type === "Path?" ||
-  type === "PublicPath" ||
-  type === "PublicPath?" ||
-  type === "PrivatePath" ||
-  type === "PrivatePath?" ||
-  type === "StoragePath" ||
-  type === "StoragePath?" ||
-  type === "CapabilityPath" ||
-  type === "CapabilityPath?"
+export const isPath = type => {
+  const clearType = stripType(type)
+  return (
+    clearType === "Path" ||
+    clearType === "PublicPath" ||
+    clearType === "PrivatePath" ||
+    clearType === "StoragePath" ||
+    clearType === "CapabilityPath"
+  )
+}
 
 export const isBasicType = type => {
-  if (wrongType(type)) return false
+  if (isWrongType(type)) return false
 
   let fixedType = type.endsWith("?") ? type.slice(0, -1) : type
   return (
@@ -59,16 +61,16 @@ export const isBasicType = type => {
 }
 
 export const isArray = type => {
-  if (wrongType(type)) return false
+  if (isWrongType(type)) return false
+  const clearType = stripType(type)
 
-  const clearType = type.replace(/\s/g, "")
   return clearType.startsWith("[") && clearType.endsWith("]")
 }
 
 export const isDictionary = type => {
-  if (wrongType(type)) return false
+  if (isWrongType(type)) return false
+  const clearType = stripType(type)
 
-  const clearType = type.replace(/\s/g, "")
   return clearType.startsWith("{") && clearType.endsWith("}")
 }
 
